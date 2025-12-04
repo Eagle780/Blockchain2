bc133b75528f8ee4a85bc4dd0c538ada64864b37
Egle <egle.gurklyte@mif.stud.vu.lt>
2025-12-04 14:30:57 +0200

    sutaisyta klaida

--- merkle.cpp
+++ merkle.cpp
@@ -1,45 +1,56 @@
 #include "header.h"
 
-shared_ptr<MerkleNode> buildMerkleTree(const vector<string>& hashes) {
-    if (hashes.empty()) {
+shared_ptr<MerkleNode> buildMerkleTree(const vector<string> &hashes)
+{
+    if (hashes.empty())
+    {
         return make_shared<MerkleNode>(stringHash(""));
     }
-    
+
     vector<shared_ptr<MerkleNode>> nodes;
-    for (const auto& hash : hashes) {
+    for (const auto &hash : hashes)
+    {
         nodes.push_back(make_shared<MerkleNode>(hash));
     }
-    
-    while (nodes.size() > 1) {
+
+    while (nodes.size() > 1)
+    {
         vector<shared_ptr<MerkleNode>> newLevel;
-        
-        for (size_t i = 0; i < nodes.size(); i += 2) {
-            if (i + 1 < nodes.size()) {
+
+        for (size_t i = 0; i < nodes.size(); i += 2)
+        {
+            if (i + 1 < nodes.size())
+            {
                 string combinedHash = stringHash(nodes[i]->hash + nodes[i + 1]->hash);
                 newLevel.push_back(make_shared<MerkleNode>(combinedHash, nodes[i], nodes[i + 1]));
-            } else {
-                
+            }
+            else
+            {
+
                 string combinedHash = stringHash(nodes[i]->hash + nodes[i]->hash);
                 newLevel.push_back(make_shared<MerkleNode>(combinedHash, nodes[i], nodes[i]));
             }
         }
-        
+
         nodes = move(newLevel);
     }
-    
+
     return nodes[0];
 }
 
-string calculateMerkleRoot(const vector<Transakcija>& transactions) {
-    if (transactions.empty()) {
+string calculateMerkleRoot(const vector<Transakcija> &transactions)
+{
+    if (transactions.empty())
+    {
         return stringHash("");
     }
-    
+
     vector<string> transactionHashes;
-    for (const auto& tx : transactions) {
-        transactionHashes.push_back(tx.getID());
+    for (const auto &tx : transactions)
+    {
+        transactionHashes.push_back(tx.getTxHash());
     }
-    
+
     auto merkleRootNode = buildMerkleTree(transactionHashes);
     return merkleRootNode->hash;
 }
